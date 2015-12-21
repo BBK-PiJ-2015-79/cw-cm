b89da898ba9c207dbfa3cfd0fa4079609cab52b2
Chris Grocott <chris.grocott+pij@gmail.com>
2015-12-21 17:12:25 +0000

    Initial test for two argument constructor in ContactImpl

---
+++ ContactTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactTest {
+	private Contact testContact;
+	
+	@Before
+	public void resetContact() {
+		testContact = null;
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTwoArgConsThrowsIAEOnFirstArg() {
+		testContact = new ContactImpl();
+	}
+}
