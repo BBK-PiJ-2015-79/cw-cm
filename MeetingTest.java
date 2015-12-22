f7bfd960f76c013c19c1057eb80c0e9990460063
Chris Grocott <chris.grocott+pij@gmail.com>
2015-12-22 22:00:50 +0000

    Added new test to make sure that FutureMeetingImpl throws an NPE when constructed with a null second parameter

--- MeetingTest.java
+++ MeetingTest.java
@@ -5,8 +5,18 @@ import java.util.*;
 public class MeetingTest {
 	private Meeting testMeeting;
 
+	@Before
+	public void resetTestMeeting() {
+		testMeeting = null;
+	}
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testMeetingThrowsIAEOnFirstParam() {
+	public void testFutureMeetingConsThrowsIAEOnFirstParam() {
 		testMeeting = new FutureMeetingImpl(0, new GregorianCalendar(), new TreeSet<Contact>());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testFutureMeetingConsThrowsNPEOnSecondParam() {
+		testMeeting = new FutureMeetingImpl(1, null, new TreeSet<Contact>());
 	}
 }
