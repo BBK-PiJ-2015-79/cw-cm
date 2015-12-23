95c4d92341cd8c0f74be57ce88fb5f18e7d8d8d5
Chris Grocott <chris.grocott+pij@gmail.com>
2015-12-23 17:22:34 +0000

    Write first test for ContactManagerImpl (addFutureMeeting needs to return an integer greater than zero) and added outline implementation to make code compile.
    Test fails, as desired.
    
    All hail the red bar.

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,6 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.*;
 
 public class ContactManagerTest {
+	private ContactManager cMTest;
+	private Calendar futureDate;
+
+	@BeforeClass
+	public void resetCMTest() {
+		cMTest = new ContactManagerImpl();
+		futureDate = new GregorianCalendar();
+	}
+
+	@Test
+	public void checkAddingNewFutureMeetingReturnsIdGreaterThanZero() {
+		Set<Contact> contactList = new HashSet<Contact>();
+		contactList.add(new ContactImpl(1, "Bill Testman"));
+		int newFMeetingId = cMTest.addFutureMeeting(contactList, futureDate);
+		assertTrue(newFMeetingId > 0);
+	}
 
 }
