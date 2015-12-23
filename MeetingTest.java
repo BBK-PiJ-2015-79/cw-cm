162721fe65afed37997b1c08d426892c86e98f54
Chris Grocott <chris.grocott+pij@gmail.com>
2015-12-23 12:40:02 +0000

    Changed previous tests to construct FutureMeetingImpl objects with a non-empty contacts list so that illegal argument exception can be tested for independently of the IAE for ID'

--- MeetingTest.java
+++ MeetingTest.java
@@ -4,24 +4,32 @@ import java.util.*;
 
 public class MeetingTest {
 	private Meeting testMeeting;
+	Set<Contact> testContactList;
 
 	@Before
 	public void resetTestMeeting() {
 		testMeeting = null;
+		testContactList = new HashSet<Contact>();
+		testContactList.add(new ContactImpl(1, "Testy Testerton"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testFutureMeetingConsThrowsIAEOnFirstParam() {
-		testMeeting = new FutureMeetingImpl(0, new GregorianCalendar(), new TreeSet<Contact>());
+		testMeeting = new FutureMeetingImpl(0, new GregorianCalendar(), testContactList);
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void testFutureMeetingConsThrowsNPEOnSecondParam() {
-		testMeeting = new FutureMeetingImpl(1, null, new TreeSet<Contact>());
+		testMeeting = new FutureMeetingImpl(1, null, testContactList);
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void testFutureMeetingConsThrowsNPEOnThirdParam() {
 		testMeeting = new FutureMeetingImpl(1, new GregorianCalendar(), null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testFutureMeetingConsThrowsIAEOnThirdParam() {
+		testMeeting = new FutureMeetingImpl(1, new GregorianCalendar(), new TreeSet<Contact>());
 	}
 }
